228912e05ed14da9ed1328e318dd8778115a505e
Rachel Chan <r.achiechan@gmail.com>
2019-11-29 12:10:35 -0500

    Updates

---
+++ precache-manifest.07dbac6378e8d9603ff132546cad2e72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1afc1b618456ea6e04e4af42ecb05874",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "342ef6da4137186f2981",
+    "url": "/project-4-assessment/static/css/main.feddc67f.chunk.css"
+  },
+  {
+    "revision": "867a0109a3e600b16f11",
+    "url": "/project-4-assessment/static/js/2.b66c033e.chunk.js"
+  },
+  {
+    "revision": "342ef6da4137186f2981",
+    "url": "/project-4-assessment/static/js/main.7a0dfff6.chunk.js"
+  },
+  {
+    "revision": "c480e96c42e26b62270f",
+    "url": "/project-4-assessment/static/js/runtime-main.4a9badbe.js"
+  }
+]);
